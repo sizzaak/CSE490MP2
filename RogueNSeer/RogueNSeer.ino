7135debcc2abf9b8cd550e03af101ea252d74296
sizzaak <sizzaak@cs.washington.edu>
2021-05-17 17:50:01 -0700

    rogue registers being hit by turning black

--- RogueNSeer/RogueNSeer.ino
+++ RogueNSeer/RogueNSeer.ino
@@ -19,6 +19,10 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // --------------------------------------------------------------------------
 
+// Vibromotor constants
+const int VIBROMOTOR_OUTPUT_PIN = 5;
+const int MAX_ANALOG_OUT = 255;
+
 // Joystick constants
 const int JOYSTICK_UPDOWN_PIN = A1;
 const int JOYSTICK_LEFTRIGHT_PIN = A0;
@@ -64,6 +68,7 @@ GAME_STATE _curState = START;
       top-right
 */
 int _attackStates[ATTACK_COUNT];
+int _earthquakeState;
 
 void setup() {
   // The following OLED setup taken from the ssd1306_128x64_i2c library example
@@ -95,7 +100,7 @@ void loop() {
   int i = 0;
   while (i <= 3) {
     if (_attackStates[i] < ATTACK_END) {
-      updateAttack(i, 20);
+      updateAttack(i, 5);
       break;
     }
     i++;
@@ -194,12 +199,26 @@ void updateRogue(int upDown, int leftRight) {
       yPos = Y_CENTER + R_PARTIAL_OFFSET;
     }
   }
+
+  // Check whether rogue has been killed
+  int attackActive[ATTACK_COUNT];
+  for (int i = 0; i < ATTACK_COUNT; i++) {
+    attackActive[i] = (_attackStates[i] > PAUSE_END + ATTACK_EDGE_LENGTH) && (_attackStates[i] < ATTACK_END - ATTACK_EDGE_LENGTH);
+  }
+  if ((upDown == 0 && attackActive[0]) || (leftRight == 0 && attackActive[1]) ||
+      (leftRight == -upDown && attackActive[2]) || (leftRight == upDown && attackActive[3])) {
+        alive = false;
+  }
   
-  drawRogue(xPos, yPos, true);
+  drawRogue(xPos, yPos, alive);
 }
 
 void drawRogue(int x, int y, bool alive) {
-  display.fillRect(x - 5, y - 5, 10, 10, SSD1306_WHITE);
+  if (alive) {
+    display.fillRect(x - 5, y - 5, 10, 10, SSD1306_WHITE);
+  } else {
+    display.drawRect(x - 5, y - 5, 10, 10, SSD1306_WHITE);
+  }
 }
 
 void drawAttackPartial(int direction, int offset) {
